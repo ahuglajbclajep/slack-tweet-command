25d3147f983852281af73876a6d406a913eda473
ahu <ahuglajbclajep@gmail.com>
2021-04-12 19:17:50 +0900

    deploy to cloudflare (#2)

---
+++ src/bindings.d.ts
@@ -0,0 +1,7 @@
+// see https://github.com/cloudflare/workers-types/tree/v2.2.0#using-bindings
+export declare global {
+  const TWITTER_CONSUMER_KEY: string;
+  const TWITTER_CONSUMER_SECRET: string;
+  const TWITTER_ACCESS_TOKEN: string;
+  const TWITTER_ACCESS_SECRET: string;
+}
